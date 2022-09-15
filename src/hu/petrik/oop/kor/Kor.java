8dbe900ce718a209d66c99d218dd119fd5526003
Füzi Zsigmond <fzsigmond02@gmail.com>
2022-09-15 10:40:05 +0200

    Kör - Inicializálás

---
+++ src/hu/petrik/oop/kor/Kor.java
@@ -0,0 +1,28 @@
+package hu.petrik.oop.kor;
+
+public class Kor {
+    private int x;
+    private int y;
+    private int r;
+
+    public Kor(int x, int y, int r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    public Kor(int r){
+        this.x = 0;
+        this.y = 0;
+        this.r = r;
+    }
+
+    public Kor(){
+        this.x = (int)(Math.random() * (2 * 100) + 1) - 100;
+        this.y = (int)(Math.random() * (2 * 100) + 1) - 100;
+        this.r = (int)(Math.random() * (2 * 100) + 1) - 100;
+    }
+
+}
+
+
