f1ed43584d0cfaed0fcbb1e2e6dc713f8219b36d
Füzi Zsigmond <fzsigmond02@gmail.com>
2022-09-15 09:34:54 +0200

    Origo távolság

--- src/hu/petrik/oop/Main.java
+++ src/hu/petrik/oop/Main.java
@@ -22,6 +22,16 @@ public class Main {
         for (Pont p: pontok) {
             System.out.println(p);
         }
+
+        //Origótól legtávolabb eső pont
+        int legtavolabbPontIndex = 0;
+        for (int i = 1; i < pontok.length; i++) {
+            if (pontok[legtavolabbPontIndex].getOrigotolMertTavolsag() < pontok[i].getOrigotolMertTavolsag()){
+                legtavolabbPontIndex = i;
+            }
+        }
+
+        System.out.printf("A legtávolabbi pont az origótól: a(z) %d pont, koordinátái: %s, origótól mért távolsága: %.3f", legtavolabbPontIndex + 1, pontok[legtavolabbPontIndex], pontok[legtavolabbPontIndex].getOrigotolMertTavolsag());
     }
 
 }
