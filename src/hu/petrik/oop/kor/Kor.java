a55d9446ee7a5faaf1f06b01639357e20dea23f2
FZsiga02 <fzsigmond02@gmail.com>
2022-09-16 16:29:22 +0200

    Kör - Méretezés

--- src/hu/petrik/oop/kor/Kor.java
+++ src/hu/petrik/oop/kor/Kor.java
@@ -11,26 +11,31 @@ public class Kor {
         this.r = r;
     }
 
-    public Kor(int r){
+    public Kor(int r) {
         this.x = 0;
         this.y = 0;
         this.r = r;
     }
 
-    public Kor(){
-        this.x = (int)(Math.random() * (2 * 100) + 1) - 100;
-        this.y = (int)(Math.random() * (2 * 100) + 1) - 100;
-        this.r = (int)(Math.random() * (2 * 100) + 1) - 100;
+    public Kor() {
+        this.x = (int) (Math.random() * (2 * 100) + 1) - 100;
+        this.y = (int) (Math.random() * (2 * 100) + 1) - 100;
+        this.r = (int) (Math.random() * (2 * 100) + 1) - 100;
     }
 
-    public double getKerulet(){
-        double kerulet = 2*r*Math.PI;
+    public double getKerulet() {
+        double kerulet = 2 * r * Math.PI;
         return kerulet;
     }
 
-    public double getTerulet(){
-        double terulet = r*r*Math.PI;
+    public double getTerulet() {
+        double terulet = r * r * Math.PI;
         return terulet;
+    }
+
+    public double getMeretezes(int meret) {
+        this.r += meret;
+        return this.r;
     }
 }
 
--- src/hu/petrik/oop/kor/Main.java
+++ src/hu/petrik/oop/kor/Main.java
@@ -1,16 +1,25 @@
 package hu.petrik.oop.kor;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         Kor k1 = new Kor(10,10,10);
 
 
         //Kör kerülete
         System.out.printf("A kör kerülete: %.2f", k1.getKerulet());
-        System.out.println(" ");
+        System.out.println("");
 
         //Kör területe
         System.out.printf("A kör területe: %.2f", k1.getTerulet());
-        System.out.println(" ");
+        System.out.println("");
+
+        //Kör méretezése
+        System.out.printf("Kérem adja meg mennyivel legyen nagyítva/kicsinyítve a kör (+/-): ");
+        System.out.println("");
+        System.out.printf("A kör új mérete: %.2f", k1.getMeretezes(sc.nextInt()));
     }
 }
